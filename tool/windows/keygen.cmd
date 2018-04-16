6bb08bfba5eee5679435488d2c14794af6af9bcc
naoki hirata <naoki@magic3.org>
2018-04-16 12:01:19 +0900

    SSHキーインストーラ追加。

--- tool/windows/keygen.cmd
+++ tool/windows/keygen.cmd
@@ -3,6 +3,7 @@
 ::
 :: 履歴
 ::  v1.0 2018/4/15 新規作成
+::  v1.1 2018/4/16 SSHキーインストーラ追加
 ::
 @echo off
 :: カレントディレクトリの設定
@@ -26,6 +27,7 @@ set SERVICE_NAME_HEAD=sakura_vps_
 set TERATERM_DIRNAME=サーバ接続
 set DEVELOP_DIR=E:\develop
 set KEY_DIR=%DEVELOP_DIR%\key\keys
+set INIT_TOOL_DIR=%DEVELOP_DIR%\key\install
 
 :: その他パラメータ
 set SERVER_ID=%1%
@@ -36,6 +38,7 @@ set KEYFILE=%KEY_DIR%\%KEY_FILENAME%
 set SSH_CONFIG=%USERPROFILE%\.ssh\config
 set TERATERM_DIR=%DEVELOP_DIR%\%TERATERM_DIRNAME%
 set TERATERM_FILE=%TERATERM_DIR%\%SERVICE_NAME%_%SERVER_ID%^(%IP%^).ttl
+set INSTALL_TOOL_FILE=%INIT_TOOL_DIR%\%SERVICE_NAME_HEAD%%SERVER_ID%^(%IP%^)_installer.sh
 
 :: キーファイルが存在する場合は終了
 if exist %KEYFILE% (
@@ -82,5 +85,20 @@ echo strconcat msg keyfile
 echo connect msg
 ) > %TERATERM_FILE%
 echo Tera Term用サーバ接続用スクリプト^(%TERATERM_FILE%^)を作成しました
+
+:: TeraTerm用設定ファイル追加
+set /p PUB_KEY_DATA=<%KEYFILE%.pub
+mkdir %INIT_TOOL_DIR%
+(
+echo #!/bin/bash
+echo set -e
+echo.
+echo # create directory
+echo mkdir -p /root/.ssh
+echo.
+echo # create key file
+echo echo "%PUB_KEY_DATA%" ^^^> /root/.ssh/authorized_keys
+echo chmod 600 /root/.ssh/authorized_keys
+) | tr -d '\r' > %INSTALL_TOOL_FILE%
 
 echo 終了
